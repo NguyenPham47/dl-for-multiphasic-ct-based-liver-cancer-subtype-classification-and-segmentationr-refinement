638cf98f5d6a0d644cc22301a2ba0bbc32e0392c
Phạm Khôi Nguyên <23521055@gm.uit.edu.vn>
2025-12-27 19:06:42 +0700

    Delete hcc_refinenet directory

--- hcc_refinenet/sanity_check.py
+++
@@ -1,3 +0,0 @@
-# After building train_loader in train.py (temporary)
-b = next(iter(train_loader))
-print("img:", b["img"].shape, "lab_sum:", b["lab"].sum().item(), "roi_sum:", b["roi"].sum().item())
--- hcc_refinenet/utils.py
+++
@@ -1,5 +0,0 @@
-import random, numpy as np, torch
-
-def seed_all(seed=2025):
-    random.seed(seed); np.random.seed(seed); torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
